b5571f3f3ff04b13dc07f723f41c49e221748436
Derek <derekrenbiz@gmail.com>
2025-11-11 04:45:55 -0800

    Update to 1.0.4 to test autoupdate

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fluz Bank Balance
 // @namespace    fluz_balance
-// @version      1.0.3
+// @version      1.0.4
 // @description  Show Fluz Bank Balance
 // @author       GammaExpansion
 // @match        https://fluz.app/manage-money*
